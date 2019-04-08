65023bc9275b00726f4f12f6ae9114bcc4607b6f
Manas Khosla <43397881+ManasKhosla@users.noreply.github.com>
2019-04-08 23:17:26 +0530

    Add files via upload

---
+++ fcfs.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	// number of processes
+	int t;
+	cout << "Enter number of processes: ";
+	cin >> t;
+	
+	// input the number of processes in array pointer
+	int *p = new int[t];
+	
+	// input process info
+	for(int i=0; i<t; i++){
+		cout << "Burst time of Process #" << i+1 << ": ";
+		cin >> p[i];
+	}
+	
+	// output info
+	cout << setw(8) << "\nProcesses | " << setw(12) << "Waiting Time | " << setw(12) << "Turnaround Time" << endl;
+	int wait = 0;	//waiting time
+	int last = 0;	//turnaround time for last process
+	int av = 0;		//average waiting time
+	// printing out the table
+	for(int j=0; j<t; j++){
+		last = p[j]+wait;
+		cout << "Process #" << j+1 << setw(12) << wait << setw(12) << last << endl;
+		av = av+wait;
+		wait = last;
+	}
+	
+	// Average waiting time
+	cout << "\nAverage Waiting time: " << av/t << endl;
+	cout << "Average Turnaround time: " << last/t << endl;
+	
+}
